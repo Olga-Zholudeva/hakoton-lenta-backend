8125a335bc97bd282f7d2693a870d09c66ea56b3
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-09-26 15:27:25 +0300

    Update users admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import LentaUser
+
+
+class UserAdmin(UserAdmin):
+    model = LentaUser
+    list_display = ('id', 'email', 'username')
+    list_filter = ('email', 'username')
+    search_fields = ('email', 'username')
+    ordering = ('username',)
+
+
+admin.site.register(LentaUser, UserAdmin)
