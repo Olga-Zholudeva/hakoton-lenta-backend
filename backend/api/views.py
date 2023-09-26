62c2c7a23e91efa6f345e94908968d6516333865
Olga-Zholudeva <joludeva.olga@yandex.ru>
2023-09-26 12:21:13 +0300

    Прописала сериализатор, вью, роутер для эндпоинта categories

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from products.models import Sku
+from api.serializers import SkuSerializer
 
-# Create your views here.
+
+class SkuViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Sku.objects.all()
+    serializer_class = SkuSerializer
