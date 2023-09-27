e5ec911c12b86d8a1a5f246f81c8d42891493eaf
Olga-Zholudeva <joludeva.olga@yandex.ru>
2023-09-27 13:52:19 +0300

    Прописала эндпоинт sales, пока без фильтрации

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,50 @@
 from rest_framework import serializers
-from products.models import Sku
+from products.models import Sku, Sales
+from django.db.models import Sum
 
 
 class SkuSerializer(serializers.ModelSerializer):
     ''''Сериализатор для модели Товар.'''
+    sku = serializers.CharField(source='pr_sku_id')
+    group = serializers.CharField(source='pr_group_id')
+    category = serializers.CharField(source='pr_cat_id')
+    subcategory = serializers.CharField(source='pr_subcat_id')
+    uom = serializers.CharField(source='pr_uom_id')
     class Meta:
         model = Sku
-        fields = '__all__'
+        fields = ('sku', 'group', 'category', 'subcategory', 'uom',)
+
+class SalesFactSerializer(serializers.ModelSerializer):
+    date = serializers.DateField()
+    sales_type = serializers.IntegerField()
+    sales_units = serializers.DecimalField(max_digits=6, decimal_places=2)
+    sales_units_promo = serializers.DecimalField(max_digits=6, decimal_places=2)
+    sales_rub = serializers.DecimalField(max_digits=8, decimal_places=2)
+    sales_run_promo = serializers.DecimalField(max_digits=8, decimal_places=2)
+
+    class Meta:
+        model = Sales
+        fields = ('date', 'sales_type', 'sales_units', 'sales_units_promo', 'sales_rub', 'sales_run_promo',)
+
+class SalesSerializer(serializers.ModelSerializer):
+    store = serializers.CharField(source='st_id')
+    sku = serializers.CharField(source='pr_sku_id')
+    fact = serializers.SerializerMethodField()
+    class Meta:
+        model = Sales
+        fields = ('store', 'sku','fact')
+
+    def get_fact(self, obj):
+        grouped_sales = Sales.objects.filter(st_id=obj.st_id, pr_sku_id=obj.pr_sku_id).values(
+            'date', 'sales_type').annotate(
+            sales_units=Sum('sales_units'),
+            sales_units_promo=Sum('sales_units_promo'),
+            sales_rub=Sum('sales_rub'),
+            sales_run_promo=Sum('sales_run_promo')
+        )
+
+        fact_serializer = SalesFactSerializer(grouped_sales, many=True)
+        return fact_serializer.data
+            
+
+
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
-from api.views import SkuViewSet
+from api.views import SkuViewSet, SalesViewSet
 
 router = DefaultRouter()
 
 router.register('categories', SkuViewSet)
+router.register('sales', SalesViewSet)
 
 urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
