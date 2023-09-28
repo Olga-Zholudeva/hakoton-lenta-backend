de6f2f2f70873021addd195da2f98c9ecb2a8f2e
K1N88 <m-end-eleev@yandex.ru>
2023-09-28 23:03:18 +0300

    devops config

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
-
 from django.urls import path, include
 from rest_framework import routers, permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# Создайте router и зарегистрируйте ваши views и url-ы
+
 router = routers.DefaultRouter(trailing_slash=False)
 
-# Определение schema_view для использования в Swagger
 schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
@@ -26,7 +24,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/', include('api.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # Путь к документации ReDoc
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc')
 ]
