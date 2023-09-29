c073a2b86b739e90b51bf76ea9ea885dc899199e
K1N88 <m-end-eleev@yandex.ru>
2023-09-29 23:04:52 +0300

    fix swagger url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,5 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
 
 from api.views import SkuViewSet, SalesViewSet, StoreViewSet, ForecastViewSet
 
@@ -14,24 +11,7 @@ router.register('sales', SalesViewSet)
 router.register('shops', StoreViewSet)
 router.register('forecast', ForecastViewSet)
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="API Documentation",
-        default_version='v1',
-        description="API documentation for Lenta project",
-        terms_of_service="https://www.example.com/policies/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 urlpatterns = [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
-         name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
-         name='schema-redoc'),
     path('auth/', include('djoser.urls.authtoken')),
     path('', include('djoser.urls')),
     path('', include(router.urls)),
