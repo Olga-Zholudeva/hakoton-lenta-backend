c5b082a74e2c5e9c388b8db274dc46f0d92b951a
K1N88 <m-end-eleev@yandex.ru>
2023-09-30 01:32:50 +0300

    fix base permission settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,7 +147,7 @@ AUTH_USER_MODEL = 'users.LentaUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
