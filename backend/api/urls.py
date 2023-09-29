28b46e386d6e3c9b17e36e2c27e5d9e3d2f7e64c
K1N88 <m-end-eleev@yandex.ru>
2023-09-29 22:26:55 +0300

    fix docker 3

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,10 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
-from api.views import SkuViewSet, SalesViewSet, StoreViewSet, ForecastViewSet
-
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+from api.views import SkuViewSet, SalesViewSet, StoreViewSet, ForecastViewSet
 
 
 router = DefaultRouter()
@@ -29,9 +28,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
     path('auth/', include('djoser.urls.authtoken')),
     path('', include('djoser.urls')),
     path('', include(router.urls)),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,15 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from users.models import LentaUser
 
 
-class UserAdmin(UserAdmin):
-    model = LentaUser
-    list_display = ('id', 'email', 'username')
+@admin.register(LentaUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'username', 'first_name', 'last_name')
     list_filter = ('email', 'username')
     search_fields = ('email', 'username')
     ordering = ('username',)
-
-
-admin.site.register(LentaUser, UserAdmin)
