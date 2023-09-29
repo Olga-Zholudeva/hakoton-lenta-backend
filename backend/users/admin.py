6b0d925ed25a38bab4a30c0452398b27854c4cd5
K1N88 <m-end-eleev@yandex.ru>
2023-09-30 01:30:00 +0300

    fix admin site2

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -6,6 +6,5 @@ from users.models import LentaUser
 @admin.register(LentaUser)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'username', 'first_name', 'last_name')
-    list_filter = ('email', 'username')
     search_fields = ('email', 'username')
     ordering = ('username',)
