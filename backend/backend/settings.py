66ec11849ff5dae1f6645dcd9c78ce40120a1565
K1N88 <m-end-eleev@yandex.ru>
2023-10-01 10:57:57 +0300

    fix cors5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -164,7 +164,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'DELETE']
 CORS_ALLOW_HEADERS = ['Authorization', 'Content-Type', 'X-CSRFToken', 'accept']
-CORS_ALLOWED_ORIGINS = ['http://web:8000', ]
+CORS_ORIGIN_WHITELIST = ['http://web:8000', ]
 '''
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', ]  # хост для фронта
 '''
