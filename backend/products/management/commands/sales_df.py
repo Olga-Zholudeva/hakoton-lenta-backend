e0660a8e92c5a266263a9d8c6fe6192c64312e3b
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-10-03 12:27:26 +0300

    Update sales_df.py

--- backend/products/management/commands/sales_df.py
+++ backend/products/management/commands/sales_df.py
@@ -3,7 +3,7 @@ import csv
 from django.core.management.base import BaseCommand
 from tqdm import tqdm
 
-from products.models import Sales, Sku, Store
+from products.models import Sales, Sku, Store, SalesFact
 from products.management.setup_logger import setup_logger
 
 
@@ -16,33 +16,46 @@ class Command(BaseCommand):
             logger.info('старт загрузки данных')
             reader = csv.reader(f)
             next(reader)
-            count = 0
             sales_list = []
-            all_store = Store.objects.all()
+            sales_fact_list = []
+            count = 0
+            all_stores = Store.objects.all()
             all_sku = Sku.objects.all()
+            all_sales = Sales.objects.all()
             for row in tqdm(reader):
                 try:
                     st_id, pr_sku_id, date, pr_sales_type_id, pr_sales_in_units, pr_promo_sales_in_units, pr_sales_in_rub, pr_promo_sales_in_rub = row
-                    store = all_store.get(pk=st_id)
+                    store = all_stores.get(pk=st_id)
                     sku = all_sku.get(pk=pr_sku_id)
-                    sales = Sales(
-                        st_id=store,
-                        pr_sku_id=sku,
-                        date=date,
+                    sale = all_sales.filter(st_id=store, pr_sku_id=sku,
+                                            date=date)
+                    if not sale:
+                        sale = Sales(
+                            st_id=store,
+                            pr_sku_id=sku,
+                            date=date,
+                        )
+                        sales_list.append(sale)
+                    sale_fact = SalesFact(
+                        st_sku_date=sale,
                         sales_type=pr_sales_type_id,
                         sales_units=pr_sales_in_units,
                         sales_units_promo=pr_promo_sales_in_units,
                         sales_rub=pr_sales_in_rub,
                         sales_run_promo=pr_promo_sales_in_rub
                     )
-                    sales_list.append(sales)
+                    sales_fact_list.append(sale_fact)
                     count += 1
-                    if count > 10000:
+                    if count > 9999:
                         Sales.objects.bulk_create(sales_list, batch_size=1000)
+                        SalesFact.objects.bulk_create(sales_fact_list,
+                                                      batch_size=1000)
                         logger.info(f'загружено {count} строк')
-                        count = 0
                         sales_list = []
+                        sales_fact_list = []
+                        count = 0
                 except Exception as error:
                     logger.error(f'сбой в работе: {error}')
             Sales.objects.bulk_create(sales_list, batch_size=1000)
+            SalesFact.objects.bulk_create(sales_fact_list, batch_size=1000)
             logger.info(f'загружено {count} строк')
