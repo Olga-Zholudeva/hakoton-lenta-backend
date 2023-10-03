7872bf60b12ca343700260ba7a142d68eab726b6
K1N88 <m-end-eleev@yandex.ru>
2023-10-03 23:55:42 +0300

    fix serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,11 +1,11 @@
-from django.db.models import Sum, Max
+from django.db.models import Max
 from rest_framework import serializers
 
-from products.models import Sku, Store, Forecast
+from products.models import Sku, Store, Forecast, SalesFact
 
 
 class SkuSerializer(serializers.ModelSerializer):
-    ''''Сериализатор для модели Товар.'''
+    ''''Сериализатор для модели Товар'''
     sku = serializers.CharField(source='pr_sku_id')
     group = serializers.CharField(source='pr_group_id')
     category = serializers.CharField(source='pr_cat_id')
@@ -18,7 +18,7 @@ class SkuSerializer(serializers.ModelSerializer):
 
 
 class StoreSerializer(serializers.ModelSerializer):
-    '''Сериализатор для модели Магазины.'''
+    '''Сериализатор для модели Магазины'''
     store = serializers.CharField(source='st_id')
     city = serializers.CharField(source='st_city_id')
     division = serializers.CharField(source='st_division_code')
@@ -43,24 +43,55 @@ class ForecastSkuSerializer(serializers.ModelSerializer):
 
 class ForecastSerializer(serializers.ModelSerializer):
     '''Сериализатор для страницы с прогнозом продаж'''
-    st_id = serializers.CharField(
+    store = serializers.PrimaryKeyRelatedField(
+        read_only=True,
         source='st_sku_date.st_id'
     )
-    pr_group_id = serializers.CharField(source='st_sku_date.pr_sku_id.pr_group_id')
-    pr_cat_id = serializers.CharField(source='st_sku_date.pr_sku_id.pr_cat_id')
-    subcategory = serializers.CharField(source='st_sku_date.pr_sku_id.pr_subcat_id')
-    pr_sku_id = serializers.CharField(source='st_sku_date.pr_sku_id')
+    group = serializers.CharField(
+        source='st_sku_date.pr_sku_id.pr_group_id'
+    )
+    category = serializers.CharField(
+        source='st_sku_date.pr_sku_id.pr_cat_id'
+    )
+    subcategory = serializers.CharField(
+        source='st_sku_date.pr_sku_id.pr_subcat_id'
+    )
+    sku = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+        source='st_sku_date.pr_sku_id'
+    )
     forecast = serializers.SerializerMethodField()
 
     class Meta:
         model = Forecast
-        fields = ('st_id', 'pr_group_id', 'pr_cat_id', 'subcategory', 'pr_sku_id', 'forecast',)
-    
+        fields = ('store', 'group', 'category', 'subcategory',
+                  'sku', 'forecast',)
+
     def get_forecast(self, obj):
-        last_forecast = Forecast.objects.aggregate(Max('forecast_date'))['forecast_date__max']
+        last_forecast = Forecast.objects.aggregate(
+            Max('forecast_date')
+        )['forecast_date__max']
         forecast = Forecast.objects.filter(
-            forecast_date = last_forecast,
+            forecast_date=last_forecast,
             st_sku_date__st_id=obj.st_sku_date.st_id,
             st_sku_date__pr_sku_id=obj.st_sku_date.pr_sku_id
         )
         return ForecastSkuSerializer(forecast, many=True).data
+
+
+class SalesSerializer(serializers.ModelSerializer):
+    '''Сериализатор факта продаж'''
+    store = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+        source='st_sku_date.st_id'
+    )
+    sku = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+        source='st_sku_date.pr_sku_id'
+    )
+    date = serializers.DateTimeField(source='st_sku_date.date')
+
+    class Meta:
+        model = SalesFact
+        fields = ('store', 'sku', 'date', 'sales_type', 'sales_units',
+                  'sales_units_promo', 'sales_rub', 'sales_run_promo')
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,36 +2,43 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import SAFE_METHODS
 
-from api.serializers import (
-                             StoreSerializer, SkuSerializer, ForecastSerializer
-                             )
+from api.serializers import (StoreSerializer, SkuSerializer, SalesSerializer,
+                             ForecastSerializer, SalesPostSerializer)
 from api.filters import ForecastFilter
-from products.models import Sku, Store, Forecast
+from products.models import Sku, Store, Forecast, SalesFact
 
 
-class SkuViewSet(
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet
-):
+class SkuViewSet(mixins.ListModelMixin,nviewsets.GenericViewSet):
     '''Обработчик для товаров.'''
     queryset = Sku.objects.all()
     serializer_class = SkuSerializer
 
-class ForecastViewSet(
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet
-):
+
+class ForecastViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     '''Обработчик для прогноза'''
     queryset = Forecast.objects.all()
     serializer_class = ForecastSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = ForecastFilter
 
-class StoreViewSet(
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet 
-):
+
+class StoreViewSet(mixins.ListModelMixin,viewsets.GenericViewSet ):
     '''Обработчик для магазинов.'''
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
 
+
+class SalesViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+    '''Обработчик для фактических продаж.'''
+    queryset = SalesFact.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = SalesFilter
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return SalesSerializer
+        return SalesPostSerializer
