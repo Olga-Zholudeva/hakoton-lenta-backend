e271fe56180851ed06fb0d9004b02ef1ca3f67a9
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-10-05 01:15:48 +0300

    Update filters.py

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import FilterSet, filters
 
-from products.models import SalesFact, Forecast
+from products.models import SalesFact, Forecast, SalesDiff
 
 
 class SalesFilter(FilterSet):
@@ -43,5 +43,27 @@ class ForecastFilter(FilterSet):
 
     class Meta:
         model = Forecast
+        fields = ['st_city_id', 'st_id', 'pr_sku_id', 'pr_group_id',
+                  'pr_cat_id', 'pr_subcat_id']
+
+
+class SalesDiffFilter(FilterSet):
+    st_city_id = filters.CharFilter(
+        field_name='st_sku_date__st_id__st_city_id'
+    )
+    st_id = filters.CharFilter(field_name='st_sku_date__st_id')
+    pr_sku_id = filters.CharFilter(field_name='st_sku_date__pr_sku_id')
+    pr_group_id = filters.CharFilter(
+        field_name='st_sku_date__pr_sku_id__pr_group_id'
+    )
+    pr_cat_id = filters.CharFilter(
+        field_name='st_sku_date__pr_sku_id__pr_cat_id'
+    )
+    pr_subcat_id = filters.CharFilter(
+        field_name='st_sku_date__pr_sku_id__pr_subcat_id'
+    )
+
+    class Meta:
+        model = SalesDiff
         fields = ['st_city_id', 'st_id', 'pr_sku_id', 'pr_group_id',
                   'pr_cat_id', 'pr_subcat_id']
