cec14cb96e532ca21ab6fcf6e3fb07911744ee69
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-10-04 13:58:11 +0300

    Update filters.py

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,26 +1,47 @@
 from django_filters.rest_framework import FilterSet, filters
 
-from products.models import Sales, ForecastSku
+from products.models import Sales, Forecast
 
 
 class SalesFilter(FilterSet):
-    st_id = filters.CharFilter()
-    pr_sku_id = filters.CharFilter(field_name='pr_sku_id__pr_group_id')
-    pr_cat_id = filters.CharFilter(field_name='pr_sku_id__pr_cat_id')
-    pr_subcat_id = filters.CharFilter(field_name='pr_sku_id__pr_subcat_id')
+    st_city_id = filters.CharFilter(
+        field_name='st_sku_date__st_id__st_city_id'
+    )
+    st_id = filters.CharFilter(field_name='st_sku_date__st_id')
+    pr_sku_id = filters.CharFilter(field_name='st_sku_date__pr_sku_id')
+    pr_group_id = filters.CharFilter(
+        field_name='st_sku_date__pr_sku_id__pr_group_id'
+    )
+    pr_cat_id = filters.CharFilter(
+        field_name='st_sku_date__pr_sku_id__pr_cat_id'
+    )
+    pr_subcat_id = filters.CharFilter(
+        field_name='st_sku_date__pr_sku_id__pr_subcat_id'
+    )
 
     class Meta:
         model = Sales
-        fields = ['st_id', 'pr_sku_id', 'pr_cat_id', 'pr_subcat_id']
+        fields = ['st_city_id', 'st_id', 'pr_sku_id', 'pr_group_id',
+                  'pr_cat_id', 'pr_subcat_id']
 
 
 class ForecastFilter(FilterSet):
-    st_city_id = filters.CharFilter(field_name='st_sku_date__st_id__st_city_id')
-    st_id = filters.CharFilter()
-    pr_sku_id = filters.CharFilter(field_name='st_sku_date__pr_sku_id__pr_group_id')
-    pr_cat_id = filters.CharFilter(field_name='st_sku_date__pr_sku_id__pr_cat_id')
-    pr_subcat_id = filters.CharFilter(field_name='st_sku_date__pr_sku_id__pr_subcat_id')
+    st_city_id = filters.CharFilter(
+        field_name='st_sku_date__st_id__st_city_id'
+    )
+    st_id = filters.CharFilter(field_name='st_sku_date__st_id')
+    pr_sku_id = filters.CharFilter(field_name='st_sku_date__pr_sku_id')
+    pr_group_id = filters.CharFilter(
+        field_name='st_sku_date__pr_sku_id__pr_group_id'
+    )
+    pr_cat_id = filters.CharFilter(
+        field_name='st_sku_date__pr_sku_id__pr_cat_id'
+    )
+    pr_subcat_id = filters.CharFilter(
+        field_name='st_sku_date__pr_sku_id__pr_subcat_id'
+    )
 
     class Meta:
         model = Forecast
-        fields = ['st_id', 'pr_sku_id', 'pr_cat_id', 'pr_subcat_id']
+        fields = ['st_city_id', 'st_id', 'pr_sku_id', 'pr_group_id',
+                  'pr_cat_id', 'pr_subcat_id']
