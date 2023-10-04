ae2e899d42229da919a68e8d11dc08275cf845de
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-10-04 10:08:35 +0300

    Update filters.py

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,17 @@
 from django_filters.rest_framework import FilterSet, filters
 
-from products.models import Forecast
+from products.models import Sales, ForecastSku
+
+
+class SalesFilter(FilterSet):
+    st_id = filters.CharFilter()
+    pr_sku_id = filters.CharFilter(field_name='pr_sku_id__pr_group_id')
+    pr_cat_id = filters.CharFilter(field_name='pr_sku_id__pr_cat_id')
+    pr_subcat_id = filters.CharFilter(field_name='pr_sku_id__pr_subcat_id')
+
+    class Meta:
+        model = Sales
+        fields = ['st_id', 'pr_sku_id', 'pr_cat_id', 'pr_subcat_id']
 
 
 class ForecastFilter(FilterSet):
